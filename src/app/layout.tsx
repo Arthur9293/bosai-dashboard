a4bf7c092b510731ca36fa889849a86681f73d50
Arthur9293 <173128448+Arthur9293@users.noreply.github.com>
2026-03-08 22:07:56 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
-import "./globals.css";
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "BOSAI",
-  description: "BOSAI SaaS V1 Dashboard",
-};
+import "../src/app/globals.css";
+import type { ReactNode } from "react";
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: ReactNode;
+}) {
   return (
     <html lang="fr">
       <body>{children}</body>
