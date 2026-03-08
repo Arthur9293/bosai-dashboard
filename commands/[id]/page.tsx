c3384bfe96183bebd540b34cac7105137e151edb
Arthur9293 <173128448+Arthur9293@users.noreply.github.com>
2026-03-08 15:30:05 +0100

    Create page.tsx

---
+++ commands/[id]/page.tsx
@@ -0,0 +1,11 @@
+import { AppShell } from "@/components/layout/app-shell";
+
+export default function CommandDetailPage() {
+  return (
+    <AppShell title="Command detail">
+      <div className="rounded-2xl border border-white/10 bg-white/5 p-5 text-white/60">
+        Détail command à brancher.
+      </div>
+    </AppShell>
+  );
+}
