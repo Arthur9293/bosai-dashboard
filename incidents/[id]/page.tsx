1a7472aafcc934f93324dc8b61b2bf8126be2695
Arthur9293 <173128448+Arthur9293@users.noreply.github.com>
2026-03-08 15:31:15 +0100

    Create page.tsx

---
+++ incidents/[id]/page.tsx
@@ -0,0 +1,11 @@
+import { AppShell } from "@/components/layout/app-shell";
+
+export default function IncidentDetailPage() {
+  return (
+    <AppShell title="Incident detail">
+      <div className="rounded-2xl border border-white/10 bg-white/5 p-5 text-white/60">
+        Détail incident à brancher.
+      </div>
+    </AppShell>
+  );
+}
