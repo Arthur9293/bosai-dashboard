f98ed605ad11b44fbffff6f3bed9dd40d1f7dcc1
Arthur9293 <173128448+Arthur9293@users.noreply.github.com>
2026-03-09 12:21:48 +0100

    Create layout.tsx

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+import { AppShell } from "../../components/layout/app-shell";
+
+type DashboardLayoutProps = {
+  children: ReactNode;
+};
+
+export default function DashboardLayout({
+  children,
+}: DashboardLayoutProps) {
+  return <AppShell>{children}</AppShell>;
+}
