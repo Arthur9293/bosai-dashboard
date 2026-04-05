606b130ef560b3da0cd0e5edb55c18c310352040
Arthur9293 <173128448+Arthur9293@users.noreply.github.com>
2026-04-05 07:12:06 +0200

    Update page.tsx

--- src/app/flows/[id]/page.tsx
+++ src/app/flows/[id]/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { notFound } from "next/navigation";
-import FlowGraphClient from "../FlowGraphClient";
+import FlowGraphClient from "./FlowGraphClient";
 import {
   fetchCommands,
   fetchFlows,
