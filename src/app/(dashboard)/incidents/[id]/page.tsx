bd0413dd2373bba443cbdfea5190a6fb9cebffc4
Arthur9293 <173128448+Arthur9293@users.noreply.github.com>
2026-04-23 00:27:40 +0200

    Update page.tsx

--- src/app/(dashboard)/incidents/[id]/page.tsx
+++ src/app/(dashboard)/incidents/[id]/page.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import Link from "next/link";
 import { cookies } from "next/headers";
 import { notFound } from "next/navigation";
 import type { ReactNode } from "react";
